3c3cc9bb4e073bbd959d53d50acbc72faf66b739
서준혁 <joon@seojunhyeog-ui-MacBookPro-2.local>
2025-11-06 19:11:16 +0900

    ..

--- rasterize_points.cu
+++ rasterize_points.cu
@@ -244,3 +244,31 @@ std::tuple<torch::Tensor, torch::Tensor> ComputeRelocationCUDA(
 	return std::make_tuple(final_opacity, final_scale);
 
 }
+
+std::tuple<torch::Tensor, torch::Tensor> SimpleComputeRelocationCUDA(
+	torch::Tensor& opacity_old,
+	torch::Tensor& scale_old,
+	torch::Tensor& N,
+	torch::Tensor& binoms,
+	const int n_max)
+{
+	const int P = opacity_old.size(0);
+  
+	torch::Tensor final_opacity = torch::full({P}, 0, opacity_old.options().dtype(torch::kFloat32));
+	torch::Tensor final_scale = torch::full({3 * P}, 0, scale_old.options().dtype(torch::kFloat32));
+
+	if(P != 0)
+	{
+		UTILS::SimpleComputeRelocation(P,
+			opacity_old.contiguous().data<float>(),
+			scale_old.contiguous().data<float>(),
+			N.contiguous().data<int>(),
+			binoms.contiguous().data<float>(),
+			n_max,
+			final_opacity.contiguous().data<float>(),
+			final_scale.contiguous().data<float>());
+	}
+
+	return std::make_tuple(final_opacity, final_scale);
+
+}
